d5890a41e044bc9c1ee3fe2edc7bef77060fb808
Lutens <blanchetu@icloud.com>
2026-06-02 18:54:37 +0800

    更新 anon key：iat 回退一年，消除时钟导致的 JWT future 错误

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_CONFIG = {
   url: 'https://easynote.brainpowerai.com.cn:8443',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzgwMjY3NzExLCJleHAiOjIwOTU2Mjc3MTF9.njqAyohThpU0bfYW7lKIj1vWCGoduQBUQvFdLpCw1Ng'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzQ4ODYxNjA0LCJleHAiOjIwOTU3NTc2MDR9.YV6dfuKhbrLnLFQm1RH8N1KwVOy78YdV_yozHW6vtq0'
 }
