79deb405428329da167e270d190a6779ab178286
Lutens <blanchetu@icloud.com>
2026-06-02 17:46:25 +0800

    前端指向自托管 HTTPS 网关 easynote.brainpowerai.com.cn:8443

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_CONFIG = {
-  url: 'http://101.34.203.35:8000',
+  url: 'https://easynote.brainpowerai.com.cn:8443',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzgwMjY3NzExLCJleHAiOjIwOTU2Mjc3MTF9.njqAyohThpU0bfYW7lKIj1vWCGoduQBUQvFdLpCw1Ng'
 }
